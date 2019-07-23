1855b21ec3f76ca2fc081785cc5a08df33db37b6
github username <github email address>
2019-07-23 21:57:25 +0100

    finished boat test lab

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,36 @@
 require 'bundler/setup'
+require 'pry'
 Bundler.require
 require_all 'app'
+
+
+s1 = Student.new("studentNick")
+s2 = Student.new("studentImran")
+s3 = Student.new("studentJack")
+s4 = Student.new("studentRod")
+s5 = Student.new("studentJosh")
+
+
+i1 = Instructor.new("Instructor Gadget")
+i2 = Instructor.new("Instructor Joseph")
+i3 = Instructor.new("Instructor Bob")
+i4 = Instructor.new("Instructor Clarence")
+
+
+bt1 = BoatingTest.new(s1, "test1", "failed", i1)
+bt2 = BoatingTest.new(s1, "test2", "failed", i1)
+bt3 = BoatingTest.new(s1, "test2", "failed", i1)
+bt4 = BoatingTest.new(s1, "test3", "pending", i1)
+bt5 = BoatingTest.new(s2, "test3", "failed", i2)
+bt6 = BoatingTest.new(s3, "test3", "failed", i2)
+bt7 = BoatingTest.new(s4, "test4", "passed", i3)
+bt8 = BoatingTest.new(s5, "test5", "failed", i3)
+
+
+
+
+
+
+
+binding.pry
+0
